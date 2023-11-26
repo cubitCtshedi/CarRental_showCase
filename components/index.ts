35effa1ce739fa6373039a2e8e7689c94493e2b1
SuperThebe <thebes@staycontactable.com>
2023-11-26 11:28:53 +0200

    initial commit

---
+++ components/index.ts
@@ -0,0 +1,21 @@
+import Hero from './Hero'
+import CustomButton from './CustomButton'
+import Navbar from './Navbar'
+import Footer from './Footer'
+import SearchBar from './SearchBar'
+import CustomFilter from './CustomFilter'
+import CarCard from './CarCard'
+import CarDetails from './CarDetails'
+import ShowMore from '@/components/ShowMore';
+
+export {
+    Hero,
+    CustomButton,
+    SearchBar,
+    CustomFilter,
+    Navbar,
+    Footer,
+    CarCard,
+    CarDetails,
+    ShowMore
+}
